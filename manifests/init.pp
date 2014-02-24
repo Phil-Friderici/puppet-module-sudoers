35fcc0ea72e7bf8893a34e49eec2fd9a2ef11e5a
Phil Friderici <phil.friderici@ericsson.com>
2014-02-24 14:28:40 +0000

    adding spec tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,6 +45,10 @@ class sudoers(
       $rules = generate( "/opt/eis_pua/bin/${fetcher}", $::hostname, $::fqdn, $::ipaddress)
       $content = template( 'sudoers/sudoers.erb' )
     }
+    'for_spec_testing_only' : {
+      $rules = "# for spec testing only, if you see this in real life you are in a mess!"
+      $content = template( 'sudoers/sudoers.erb' )
+    }
     default : {
       fail( "Sorry, I don't know how to handle ${source} yet." )
     }
